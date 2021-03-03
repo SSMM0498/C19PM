5fa44293089f6a3c9c1390c52f20fd63f9ac955d
warnerb47 <warnerbang@gmail.com>
2021-03-03 01:17:15 +0000

    add virtual DataAcquisition environment

--- DataAcquisition/DataExtractor.py
+++ DataAcquisition/DataExtractor.py
@@ -84,3 +84,8 @@ def getCasCom(text):
         if(word.isdigit()):
             number = int(word)
     return  number
+
+# pip freeze > requirements.txt
+# pip install -r requirements.txt
+
+# Create Docker

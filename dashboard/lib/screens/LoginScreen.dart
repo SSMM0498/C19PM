5c2a9acb6c9e5021fb31b05afae9f56e6a37a68c
SSMM0498 <barsaliou1998@gmail.com>
2021-03-25 18:51:06 +0000

    Merge branch 'master' of https://github.com/SSMM0498/C19PM

--- dashboard/lib/screens/LoginScreen.dart
+++ dashboard/lib/screens/LoginScreen.dart
@@ -3,6 +3,8 @@ import 'package:covid19_progression_modeler/models/User.dart';
 import 'package:covid19_progression_modeler/screens/HomeScreen.dart';
 import 'package:covid19_progression_modeler/widgets/FadeAnimation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:starflut/starflut.dart';
 
 class LoginScreen extends StatefulWidget {
   final User currentUser = new User();
@@ -13,6 +15,13 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final User user = User();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -148,7 +157,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             },
                             child: Center(
                               child: Text(
-                                "Se connecter",
+                                'Se connecter',
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
@@ -174,12 +183,97 @@ class _LoginScreenState extends State<LoginScreen> {
   void onSubmit(BuildContext context, User user) async {
     print(user.login);
     print(user.password);
-    
+
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => HomeScreen(),
       ),
     );
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    try {
+      String Path1 = await Starflut.getResourcePath();
+      String Path2 = await Starflut.getAssetsPath();
+      StarCoreFactory starcore = await Starflut.getFactory();
+      StarServiceClass Service =
+          await starcore.initSimple("test", "123", 0, 0, []);
+      await starcore.regMsgCallBackP(
+          (int serviceGroupID, int uMsg, Object wParam, Object lParam) async {
+        print("$serviceGroupID  $uMsg   $wParam   $lParam");
+        return null;
+      });
+      StarSrvGroupClass SrvGroup = await Service["_ServiceGroup"];
+
+      /*--macos--*/
+      int Platform = await Starflut.getPlatform();
+      if (Platform == Starflut.MACOS) {
+        await starcore
+            .setShareLibraryPath(Path1); //set path for interface library
+        bool LoadResult =
+            await Starflut.loadLibrary(Path1 + "/libpython3.9.dylib");
+        print("$LoadResult"); //--load
+        await Starflut.setEnv("PYTHONPATH",
+            "/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9");
+        String pypath = await Starflut.getEnv("PYTHONPATH");
+        print("$pypath");
+      } else if (Platform == Starflut.WINDOWS) {
+        await starcore.setShareLibraryPath(
+            Path1.replaceAll("\\", "/")); //set path for interface library
+      }
+
+      String docPath = await Starflut.getDocumentPath();
+      print("docPath = $docPath");
+
+      String resPath = await Starflut.getResourcePath();
+      print("resPath = $resPath");
+
+      String assetsPath = await Starflut.getAssetsPath();
+      print("assetsPath = $assetsPath");
+
+      dynamic rr1 = await SrvGroup.initRaw("python39", Service);
+
+      print("initRaw = $rr1");
+      var Result = await SrvGroup.loadRawModule("python", "",
+          assetsPath + "/flutter_assets/starfiles/" + "testpy.py", false);
+      print("loadRawModule = $Result");
+
+      dynamic python =
+          await Service.importRawContext(null, "python", "", false, "");
+      print("python = " + await python.getString());
+
+      StarObjectClass retobj = await python.call("tt", ["hello ", "world"]);
+      print(await retobj[0]);
+      print(await retobj[1]);
+
+      print(await python["g1"]);
+
+      StarObjectClass yy = await python.call("yy", ["hello ", "world", 123]);
+      print(await yy.call("__len__", []));
+
+      StarObjectClass multiply =
+          await Service.importRawContext(null, "python", "Multiply", true, "");
+      StarObjectClass multiply_inst =
+          await multiply.newObject(["", "", 33, 44]);
+      print(await multiply_inst.getString());
+
+      print(await multiply_inst.call("multiply", [11, 22]));
+
+      await SrvGroup.clearService();
+      await starcore.moduleExit();
+
+      print("finish");
+    } on PlatformException catch (e) {
+      print("{$e.message}");
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
   }
 }

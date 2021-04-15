711ad47d9fa481151e17ac20126e3077b3232c0e
SSMM0498 <barsaliou1998@gmail.com>
2021-04-15 20:56:41 +0000

    update : evolutionStat

--- database/database.sql
+++ database/database.sql
@@ -12,57 +12,57 @@ CREATE TABLE IF NOT EXISTS locality (
 );
 
 INSERT INTO locality (localityName, nbPopulation) VALUES
-("dakar",934498),
-("guédiawaye",155279),
-("pikine",773494),
-("rufisque",688808),
-("bambey",664383),
-("diourbel",922148),
-("mbacké",94691),
-("fatick",513367),
-("foundiougne",10118),
-("gossas",415224),
-("kaffrine",166659),
-("birkilane",143479),
-("koungheul",430154),
-("malem hoddar",349777),
-("guinguinéo",111222),
-("kaolack",598377),
-("nioro du rip",915994),
-("kédougou",10056),
-("salémata",17501),
-("saraya",29484),
-("kolda",676455),
-("médina yoro foulah",148268),
-("vélingara",61835),
-("kébémer",741693),
-("linguère",712343),
-("louga",851889),
-("kanel",953308),
-("matam",241231),
-("ranérou ferlo",25426),
-("dagana",498771),
-("podor",493472),
-("saint-louis",771323),
-("bounkiling",591804),
-("goudomp",683051),
-("sédhiou",283218),
-("bakel",216972),
-("goudiry",691342),
-("koumpentoum",625333),
-("tambacounda",341645),
-("m'bour",675545),
-("thiès",353359),
-("tivaouane",200501),
-("bignona",260215),
-("oussouye",10805),
-("ziguinchor",343291);
+("dakar",9344),
+("guédiawaye",1552),
+("pikine",7734),
+("rufisque",6888),
+("bambey",6643),
+("diourbel",9221),
+("mbacké",946),
+("fatick",5133),
+("foundiougne",101),
+("gossas",4152),
+("kaffrine",1666),
+("birkilane",1434),
+("koungheul",4301),
+("malem hoddar",3497),
+("guinguinéo",1112),
+("kaolack",5983),
+("nioro du rip",9159),
+("kédougou",100),
+("salémata",175),
+("saraya",294),
+("kolda",6764),
+("médina yoro foulah",1482),
+("vélingara",618),
+("kébémer",7416),
+("linguère",7123),
+("louga",8518),
+("kanel",9533),
+("matam",2412),
+("ranérou ferlo",254),
+("dagana",4987),
+("podor",4934),
+("saint-louis",7713),
+("bounkiling",5918),
+("goudomp",6830),
+("sédhiou",2832),
+("bakel",2169),
+("goudiry",6913),
+("koumpentoum",6253),
+("tambacounda",3416),
+("m'bour",6755),
+("thiès",3533),
+("tivaouane",2005),
+("bignona",2602),
+("oussouye",108),
+("ziguinchor",3432);
 
 DROP TABLE IF EXISTS distance;
 
 CREATE TABLE IF NOT EXISTS distance (
   idDistance INT NOT NULL AUTO_INCREMENT,
-  distance INT NULL,
+  distance FLOAT NULL,
   idLocality1 INT NOT NULL,
   idLocality2 INT NOT NULL,
   PRIMARY KEY (idDistance),
@@ -93,7 +93,7 @@ DROP TABLE IF EXISTS dayStat;
 
 CREATE TABLE IF NOT EXISTS dayStat (
   idDay INT NOT NULL AUTO_INCREMENT,
-  annoucementDate INT NULL,
+  annoucementDate DATE NULL,
   numberOfTests INT NULL,
   numberOfNewCases INT NULL,
   numberOfContactCases INT NULL,
@@ -108,8 +108,48 @@ CREATE TABLE IF NOT EXISTS dayStat (
 
 INSERT INTO dayStat(annoucementDate, numberOfTests, numberOfNewCases, numberOfContactCases,numberOfCommunityCases,numberOfHealed,numberOfDeaths,
  idMonth, extractionDate) VALUES
-(01, 68, 8, 15, 78, 35, 72, 1, NOW()),(02, 52, 10, 4, 96, 5, 79, 2, NOW()),(03, 88, 10, 13, 76, 7, 93, 1, NOW()),(04, 35, 50, 77, 11, 62, 57, 2, NOW()),(05, 96, 59, 15, 73, 16, 74, 2, NOW()),(06, 84, 50, 5, 4, 9, 91, 1, NOW()),(07, 68, 71, 26, 99, 44, 47, 1, NOW()),(08, 13, 41, 89, 2, 52, 90, 1, NOW()),(09, 35, 44, 45, 59, 65, 45, 2, NOW()),(10, 49, 88, 64, 62, 71, 26, 1, NOW()),(11, 81, 41, 13, 1, 88, 74, 1, NOW()),(12, 2, 2, 5, 57, 18, 42, 1, NOW()),(13, 32, 27, 50, 83, 70, 20, 1, NOW()),(14, 12, 10, 5, 25, 19, 36, 2, NOW()),(15, 75, 16, 22, 30, 97, 50, 2, NOW()),(16, 19, 16, 11, 26, 82, 32, 1, NOW()),(17, 66, 47, 81, 48, 44, 31, 1, NOW()),(18, 57, 18, 39, 70, 96, 37, 1, NOW()),(19, 20, 9, 73, 29, 57, 90, 2, NOW()),(20, 23, 56, 40, 35, 7, 13, 2, NOW()),(21, 80, 22, 51, 83, 3, 25, 1, NOW()),(22, 77, 14, 7, 70, 2, 16, 2, NOW()),(23, 68, 10, 30, 87, 25, 36, 1, NOW()),(24, 71, 46, 63, 63, 24, 46, 2, NOW()),(25, 52, 2, 53, 3, 89, 99, 1, NOW()),(26, 33, 35, 65, 28, 24, 51, 1, NOW()),(27, 37, 52, 58, 23, 92, 45, 1, NOW()),(28, 83, 68, 7, 18, 4, 62, 1, NOW()),(29, 85, 40, 74, 30, 4, 91, 1, NOW()),(30, 11, 47, 39, 96, 93, 34, 2, NOW()),(05, 96, 59, 15, 73, 16, 74, 3, NOW()),(06, 84, 50, 5, 4, 9, 91, 3, NOW()),(07, 68, 71, 26, 99, 44, 47, 3, NOW()),(08, 13, 41, 89, 2, 52, 90, 3, NOW()),(09, 35, 44, 45, 59, 65, 45, 3, NOW()),(21, 80, 22, 51, 83, 3, 25, 3, NOW()),(22, 77, 14, 7, 70, 2, 16, 3, NOW()),(23, 68, 10, 30, 87, 25, 36, 3, NOW()),(24, 71, 46, 63, 63, 24, 46, 3, NOW()),(25, 52, 2, 53, 3, 89, 99, 3, NOW()),(26, 33, 35, 65, 28, 24, 51, 3, NOW()),
-(31, 26, 23, 64, 12, 43, 50, 3, NOW());
+('2020-01-01', 68, 8, 15, 78, 35, 72, 1, NOW()),
+('2020-01-03', 88, 10, 13, 76, 7, 93, 1, NOW()),
+('2020-01-06', 84, 50, 5, 4, 9, 91, 1, NOW()),
+('2020-01-07', 68, 71, 26, 99, 44, 47, 1, NOW()),
+('2020-01-08', 13, 41, 89, 2, 52, 90, 1, NOW()),
+('2020-01-10', 49, 88, 64, 62, 71, 26, 1, NOW()),
+('2020-01-11', 81, 41, 13, 1, 88, 74, 1, NOW()),
+('2020-01-12', 2, 2, 5, 57, 18, 42, 1, NOW()),
+('2020-01-13', 32, 27, 50, 83, 70, 20, 1, NOW()),
+('2020-01-16', 19, 16, 11, 26, 82, 32, 1, NOW()),
+('2020-01-17', 66, 47, 81, 48, 44, 31, 1, NOW()),
+('2020-01-18', 57, 18, 39, 70, 96, 37, 1, NOW()),
+('2020-01-21', 80, 22, 51, 83, 3, 25, 1, NOW()),
+('2020-01-23', 68, 10, 30, 87, 25, 36, 1, NOW()),
+('2020-01-25', 52, 2, 53, 3, 89, 99, 1, NOW()),
+('2020-01-26', 33, 35, 65, 28, 24, 51, 1, NOW()),
+('2020-01-27', 37, 52, 58, 23, 92, 45, 1, NOW()),
+('2020-01-28', 83, 68, 7, 18, 4, 62, 1, NOW()),
+('2020-01-29', 85, 40, 74, 30, 4, 91, 1, NOW()),
+('2020-01-30', 11, 47, 39, 96, 93, 34, 1, NOW()),
+('2020-05-02', 52, 10, 4, 96, 5, 79, 2, NOW()),
+('2020-05-04', 35, 50, 77, 11, 62, 57, 2, NOW()),
+('2020-05-05', 96, 59, 15, 73, 16, 74, 2, NOW()),
+('2020-05-09', 35, 44, 45, 59, 65, 45, 2, NOW()),
+('2020-05-14', 12, 10, 5, 25, 19, 36, 2, NOW()),
+('2020-05-15', 75, 16, 22, 30, 97, 50, 2, NOW()),
+('2020-05-19', 20, 9, 73, 29, 57, 90, 2, NOW()),
+('2020-05-20', 23, 56, 40, 35, 7, 13, 2, NOW()),
+('2020-05-22', 77, 14, 7, 70, 2, 16, 2, NOW()),
+('2020-05-24', 71, 46, 63, 63, 24, 46, 2, NOW()),
+('2020-06-05', 96, 59, 15, 73, 16, 74, 3, NOW()),
+('2020-06-06', 84, 50, 5, 4, 9, 91, 3, NOW()),
+('2020-06-07', 68, 71, 26, 99, 44, 47, 3, NOW()),
+('2020-06-08', 13, 41, 89, 2, 52, 90, 3, NOW()),
+('2020-06-09', 35, 44, 45, 59, 65, 45, 3, NOW()),
+('2020-06-21', 80, 22, 51, 83, 3, 25, 3, NOW()),
+('2020-06-22', 77, 14, 7, 70, 2, 16, 3, NOW()),
+('2020-06-23', 68, 10, 30, 87, 25, 36, 3, NOW()),
+('2020-06-24', 71, 46, 63, 63, 24, 46, 3, NOW()),
+('2020-06-25', 52, 2, 53, 3, 89, 99, 3, NOW()),
+('2020-06-26', 33, 35, 65, 28, 24, 51, 3, NOW()),
+('2020-06-30', 26, 23, 64, 12, 43, 50, 3, NOW());
 
 DROP TABLE IF EXISTS localityStat;
 
@@ -124,125 +164,255 @@ CREATE TABLE IF NOT EXISTS localityStat (
   CONSTRAINT fk_locs_loc FOREIGN KEY (idLocality) REFERENCES locality (idLocality)
 );
 
-DROP TABLE IF EXISTS EvolutionStat;
-
-CREATE TABLE IF NOT EXISTS EvolutionStat (
-  idEvol INT NOT NULL AUTO_INCREMENT,
-  Conc FLOAT,
-  Prog FLOAT NULL DEFAULT 0,
-  idMonth INT NOT NULL,
-  idLocality INT NOT NULL,
-  PRIMARY KEY (idEvol),
-  CONSTRAINT fk_ev_mont FOREIGN KEY (idMonth) REFERENCES monthStat (idMonth),
-  CONSTRAINT fk_ev_loc FOREIGN KEY (idLocality) REFERENCES locality (idLocality)
-);
-
-DROP TABLE IF EXISTS TransmissionLevel;
-
-CREATE TABLE IF NOT EXISTS TransmissionLevel (
-  idTrans INT NOT NULL AUTO_INCREMENT,
-  transValue FLOAT NULL,
-  idMonth INT NOT NULL,
-  idLocality1 INT NOT NULL,
-  idLocality2 INT NOT NULL,
-  PRIMARY KEY (idTrans),
-  CONSTRAINT fk_trans_mont FOREIGN KEY (idMonth) REFERENCES monthStat (idMonth),
-  CONSTRAINT fk_trans_loc1 FOREIGN KEY (idLocality1) REFERENCES locality (idLocality),
-  CONSTRAINT fk_trans_loc2 FOREIGN KEY (idLocality2) REFERENCES locality (idLocality)
-);
-
-
-CREATE TRIGGER calculate_evolution_values
-AFTER INSERT
-ON localityStat FOR EACH ROW
+CREATE FUNCTION getPopulation (idL INT)
+RETURNS INT
+DETERMINISTIC
 BEGIN
-    DECLARE var_nbPopulation INT;
+  DECLARE v_nbPopulation INT;
 
-    DECLARE var_exist INT;
+  SELECT nbPopulation FROM locality WHERE idLocality = idL
+  INTO v_nbPopulation;
 
-    DECLARE var_isFirstCase INT;
+  RETURN v_nbPopulation;
+END;
 
-    DECLARE var_idMonth_Current INT;
-    DECLARE var_month_Current VARCHAR(7);
-    DECLARE var_idMonth_Previous INT;
+CREATE VIEW ConcentrationCumul AS
+SELECT idLocality, t1.annoucementDate, t1.newCases, (
+    SELECT SUM(newCases) / getPopulation(t1.idLocality)
+    FROM localityStat
+    NATURAL JOIN dayStat
+    WHERE idLocality = t1.idLocality
+    AND annoucementDate <= t1.annoucementDate
+) AS Conc FROM (
+    SELECT * FROM localityStat NATURAL JOIN dayStat
+    WHERE idLocality IN (
+        SELECT idLocality FROM localityStat
+    )
+    AND DATE_FORMAT(annoucementDate, '%Y-%m') IN (SELECT sourceFileName FROM monthStat)
+    ORDER BY idLocality, annoucementDate
+) t1;
 
-    DECLARE var_Conc_Current FLOAT;
-    DECLARE var_Conc_Previous FLOAT;
-    DECLARE var_Prog_Current FLOAT;
+CREATE FUNCTION TauxProgression(
+    v_idLocality INT,
+    v_Conc INT,
+    v_currentDATE DATE
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE v_Conc_Previous FLOAT;
 
-    SELECT nbPopulation FROM locality WHERE idLocality = NEW.idLocality
-    INTO var_nbPopulation;
+    SET v_Conc_Previous := 0;
 
-    SELECT idMonth, sourceFileName FROM monthStat WHERE idMonth = (
-        SELECT idMonth FROM dayStat WHERE idDay = NEW.idDay
-    ) INTO var_idMonth_Current, var_month_Current;
+    SELECT (MAX(Conc) - v_Conc) / getPopulation(idLocality) FROM ConcentrationCumul WHERE idLocality = 1
+    AND annoucementDate <= v_currentDATE
+    GROUP BY DATE_FORMAT(annoucementDate, '%Y-%m') ORDER BY DATE_FORMAT(annoucementDate, '%Y-%m') DESC LIMIT 1, 1
+    INTO v_Conc_Previous;
 
-    SET var_exist := 0;
-
-    SELECT 1, Conc FROM EvolutionStat WHERE idMonth = var_idMonth_Current AND idLocality = NEW.idLocality
-    INTO var_exist, var_Conc_Current;
-
-    SET var_idMonth_Previous := 0;
-
-    SELECT idMonth FROM monthStat WHERE sourceFileName < var_month_Current ORDER BY sourceFileName DESC LIMIT 1
-    INTO var_idMonth_Previous;
-
-    SET var_Prog_Current := 0;
-    SET var_Conc_Previous := 0;
-
-    IF (var_exist = 0) THEN
-        SET var_Conc_Current := (NEW.newCases / var_nbPopulation);
-
-        IF (var_idMonth_Previous <> 0) THEN
-
-            SELECT Conc FROM EvolutionStat WHERE idMonth = var_idMonth_Previous AND idLocality = NEW.idLocality
-            INTO var_Conc_Previous;
-
-            SET var_Prog_Current := ((var_Conc_Current - var_Conc_Previous) / var_nbPopulation);
-
-        END IF;
-
-        INSERT INTO EvolutionStat(Conc, Prog, idMonth, idLocality)
-        VALUES (var_Conc_Current, var_Prog_Current, var_idMonth_Current, NEW.idLocality);
-
-    ELSE
-
-        SET var_Conc_Current := (var_Conc_Current + NEW.newCases) / var_nbPopulation;
-
-        IF (var_idMonth_Previous <> 0) THEN
-
-            SELECT Conc FROM EvolutionStat WHERE idMonth = var_idMonth_Previous AND idLocality = NEW.idLocality
-            INTO var_Conc_Previous;
-
-            SET var_Prog_Current := ((var_Conc_Current - var_Conc_Previous) / var_nbPopulation);
-
-        END IF;
-
-        UPDATE EvolutionStat SET Conc=var_Conc_Current, Prog=var_Prog_Current
-        WHERE idMonth = var_idMonth_Current AND idLocality = NEW.idLocality;
-
-    END IF;
+    RETURN v_Conc_Previous;
 
 END;
 
-INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
-(1, 1, 20),(2, 8, 15),(2, 1, 8),(2, 44, 14),(3, 9, 4),(4, 1, 8),(4, 5, 6),(4, 44, 3),(5, 22, 9),(6, 17, 10),(6, 1, 3),(6, 37, 15),(6, 13, 14),(7, 21, 16),(8, 33, 3),(8, 20, 15),(8, 43, 1),(8, 28, 8),(8, 1, 10),(8, 36, 2),(10, 15, 3),(11, 7, 3),(11, 7, 13),(11, 11, 16),(11, 10, 5),(11, 1, 11),(11, 3, 1),(11, 16, 3),(12, 21, 10),(12, 38, 8),(13, 8, 6),(13, 18, 8),(13, 4, 2),(13, 14, 5),(13, 4, 15),(13, 36, 6),(13, 1, 3),(14, 5, 14),(14, 3, 13),(14, 10, 16),(14, 38, 2),(14, 17, 9),(15, 2, 14),(15, 45, 7),(15, 21, 4),(15, 19, 2),(15, 42, 5),(17, 16, 12),(17, 39, 10),(17, 17, 16),(17, 12, 8),(18, 24, 12),(18, 27, 13),(19, 24, 7),(19, 23, 11),(19, 1, 4),(19, 32, 1),(19, 34, 7),(20, 29, 2),(20, 31, 8),(20, 14, 3),(20, 35, 12),(20, 26, 9),(20, 13, 2),(20, 14, 1),(20, 1, 13),(21, 45, 3),(21, 4, 7),(21, 13, 2),(21, 5, 2),(21, 5, 6),(21, 1, 6),(21, 22, 6),(23, 16, 10),(23, 25, 15),(24, 45, 8),(24, 9, 13),(24, 32, 8),(24, 7, 16),(24, 40, 12),(24, 9, 4),(24, 2, 16),(24, 1, 11),(25, 11, 14),(25, 13, 8),(26, 42, 4),(27, 32, 7),(27, 2, 6),(27, 25, 2),(30, 5, 9),(30, 25, 1),(30, 1, 11),(30, 37, 12),(30, 34, 16),(30, 12, 6),(30, 44, 7),(31, 35, 4),(31, 1, 13),(31, 13, 4),(31, 32, 4),(31, 41, 11),(31, 1, 8),(32, 8, 2),(33, 7, 16),(33, 8, 11),(33, 42, 15),(33, 12, 3),(34, 1, 12),(34, 38, 2),(34, 43, 8),(34, 13, 3),(34, 18, 2),(35, 9, 6),(38, 12, 3),(38, 1, 6),(38, 22, 12),(38, 31, 11),(38, 40, 12),(38, 18, 3),(40, 29, 16),(40, 33, 13),(40, 22, 4),(40, 33, 8),(40, 36, 13),(40, 42, 13),(40, 4, 11),(41, 1, 2),(41, 22, 12),(41, 13, 1),(41, 18, 5),(41, 40, 3),(41, 2, 5),(41, 31, 9),(42, 39, 13);
+CREATE VIEW EvolutionStat AS
+SELECT idLocality, annoucementDate, newCases, Conc, TauxProgression(idLocality, Conc, annoucementDate) AS Prog FROM ConcentrationCumul;
 
--- recupérer les statistiques de tous les regions
-SELECT localityName,
-newCases,
-numberOfTests,
-numberOfContactCases,
-numberOfCommunityCases,
-numberOfHealed,
-numberOfDeaths,
-nbPopulation,
-annoucementDate FROM
-(
-  SELECT *
-  FROM localityStat
-  NATURAL JOIN locality
-) r1
-NATURAL JOIN dayStat;
-WHERE extractionDate = '2021-04-12'
-\G;
+DROP TABLE IF EXISTS TransmissionScenario;
+
+CREATE TABLE IF NOT EXISTS TransmissionScenario (
+  idContamin INT NOT NULL AUTO_INCREMENT,
+  contaminationDate DATE NOT NULL,
+  idOrigin INT NOT NULL,
+  PRIMARY KEY (idContamin, idOrigin),
+  CONSTRAINT fk_trans_cont FOREIGN KEY (idContamin) REFERENCES locality (idLocality),
+  CONSTRAINT fk_trans_orig FOREIGN KEY (idOrigin) REFERENCES locality (idLocality)
+);
+
+CREATE FUNCTION getDistance (id1 INT, id2 INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+  DECLARE v_distance FLOAT;
+
+  SELECT distance FROM distance WHERE (idLocality1 = id1 AND idLocality2 = id2) OR (idLocality2 = id1 OR idLocality1 = id1)
+  INTO v_distance;
+
+  RETURN v_distance;
+END;
+
+-- CREATE PROCEDURE generateScenario()
+-- BEGIN
+--     DECLARE c_
+-- END;
+
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(1, 2, 2),
+(1, 10, 7),
+(1, 1, 20);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(2, 8, 15),
+(2, 1, 8),
+(2, 44, 14);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(4, 1, 14),
+(4, 2, 4),
+(4, 9, 8);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(37, 1, 8),
+(37, 5, 6),
+(37, 44, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(5, 22, 9);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(36, 17, 10),
+(36, 1, 3),
+(36, 37, 15),
+(36, 13, 14);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(7, 21, 16);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(8, 33, 3),
+(8, 20, 15),
+(8, 43, 1),
+(8, 28, 8),
+(8, 1, 10),
+(8, 36, 2);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(10, 15, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(11, 7, 3),
+(11, 7, 13),
+(11, 11, 16),
+(11, 10, 5),
+(11, 1, 11),
+(11, 3, 1),
+(11, 16, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(12, 21, 10),
+(12, 38, 8);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(13, 8, 6),
+(13, 18, 8),
+(13, 4, 2),
+(13, 14, 5),
+(13, 4, 15),
+(13, 36, 6),
+(13, 1, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(14, 5, 14),
+(14, 3, 13),
+(14, 10, 16),
+(14, 38, 2),
+(14, 17, 9);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(15, 2, 14),
+(15, 45, 7),
+(15, 21, 4),
+(15, 19, 2),
+(15, 42, 5);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(17, 16, 12),
+(17, 39, 10),
+(17, 17, 16),
+(17, 12, 8);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(18, 24, 12),
+(18, 27, 13);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(19, 24, 7),
+(19, 23, 11),
+(19, 1, 4),
+(19, 32, 1),
+(19, 34, 7);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(20, 29, 2),
+(20, 31, 8),
+(20, 14, 3),
+(20, 35, 12),
+(20, 26, 9),
+(20, 13, 2),
+(20, 14, 1),
+(20, 1, 13);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(21, 45, 3),
+(21, 4, 7),
+(21, 13, 2),
+(21, 5, 2),
+(21, 5, 6),
+(21, 1, 6),
+(21, 22, 6);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(23, 16, 10),
+(23, 25, 15);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(24, 45, 8),
+(24, 9, 13),
+(24, 32, 8),
+(24, 7, 16),
+(24, 40, 12),
+(24, 9, 4),
+(24, 2, 16),
+(24, 1, 11);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(25, 11, 14),
+(25, 13, 8);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(26, 42, 4);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(27, 32, 7),
+(27, 2, 6),
+(27, 25, 2);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(30, 5, 9),
+(30, 25, 1),
+(30, 1, 11),
+(30, 37, 12),
+(30, 34, 16),
+(30, 12, 6),
+(30, 44, 7);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(31, 35, 4),
+(31, 1, 13),
+(31, 13, 4),
+(31, 32, 4),
+(31, 41, 11),
+(31, 1, 8);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(32, 8, 2);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(33, 7, 16),
+(33, 8, 11),
+(33, 42, 15),
+(33, 12, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(34, 1, 12),
+(34, 38, 2),
+(34, 43, 8),
+(34, 13, 3),
+(34, 18, 2);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(35, 9, 6);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(38, 12, 3),
+(38, 1, 6),
+(38, 22, 12),
+(38, 31, 11),
+(38, 40, 12),
+(38, 18, 3);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(40, 29, 16),
+(40, 33, 13),
+(40, 22, 4),
+(40, 33, 8),
+(40, 36, 13),
+(40, 42, 13),
+(40, 4, 11);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(41, 1, 2),
+(41, 22, 12),
+(41, 13, 1),
+(41, 18, 5),
+(41, 40, 3),
+(41, 2, 5),
+(41, 31, 9);
+INSERT INTO localityStat(idDay, idLocality, newCases) VALUES
+(42, 39, 13);

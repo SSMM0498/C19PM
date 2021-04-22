85ce1867aa616a8920ca109af3fa4e14706e8f9c
SSMM0498 <barsaliou1998@gmail.com>
2021-04-22 13:09:58 +0000

    impl : transaction duplication handler

--- dashboard/lib/utils/DataLoader.dart
+++ dashboard/lib/utils/DataLoader.dart
@@ -1,24 +1,66 @@
+import 'package:covid19_progression_modeler/config/config.dart';
+import 'package:covid19_progression_modeler/widgets/popup/ValidatorPopup.dart';
+import 'package:flutter/material.dart';
 import 'package:mysql1/mysql1.dart';
 import 'package:covid19_progression_modeler/models/models.dart';
 
-void send(List<Day> days, Set<Month> month, bool isTransaction) async {
-  var settings = new ConnectionSettings(
-    host: 'localhost',
-    port: 3306,
-    user: 'userc19pm',
-    password: 'passer',
-    db: 'c19pm',
+Future<Widget> send(
+  List<Day> days,
+  Set<Month> month,
+  bool isTransaction,
+  BuildContext context,
+) async {
+  var conn = await MysqlConfig.newConnection();
+
+  if (isTransaction) {
+    await conn.query('START TRANSACTION');
+    try {
+      await loadMonth(conn, days, month, context);
+      return showDialog(
+        context: context,
+        builder: (ctx) => ValidatorPopup(
+          title: "Mode Transactionnel",
+          message: "Le chargement des données a été effectué avec succés.",
+          question: "Voulez vous valider le processus ?",
+          okCallback: () async {
+            await conn.query('COMMIT');
+          },
+          noCallback: () async {
+            await conn.query('ROLLBACK');
+          },
+        ),
+      );
+    } catch (e) {
+      await conn.query('ROLLBACK');
+    }
+  } else {
+    await loadMonth(conn, days, month, context);
+  }
+
+  // await conn.close();
+  return showDialog(
+    context: context,
+    builder: (ctx) => ValidatorPopup(
+      title: "Chargement des données",
+      message:
+          "Le chargement des données est terminé et a été effectué avec succés.",
+    ),
   );
+}
 
-  var conn = await MySqlConnection.connect(settings);
-
+Future<void> loadMonth(
+  MySqlConnection conn,
+  List<Day> days,
+  Set<Month> month,
+  BuildContext context,
+) async {
   for (var m in month) {
     var r = await conn.query(
-        'SELECT idMonth FROM monthStat WHERE sourceFileName = ?',
-        [m.sourceFileName]);
-    int idm;
+      'SELECT idMonth FROM monthStat WHERE sourceFileName = ?',
+      [m.sourceFileName],
+    );
     if (r.isEmpty) {
-      var result = await conn.query(
+      await conn.query(
         'INSERT INTO monthStat (totalCases, totalHealed, totalDeath, sourceFileName) VALUES (?,?,?,?)',
         [
           m.totalCases,
@@ -27,59 +69,82 @@ void send(List<Day> days, Set<Month> month, bool isTransaction) async {
           m.sourceFileName,
         ],
       );
-      idm = result.insertId;
-    } else {
-      var result = await conn.query(
-        'UPDATE monthStat SET totalCases = totalCases + ?,totalHealed = totalHealed + ?,totalDeath = totalDeath + ? WHERE idMonth = ?',
-        [
-          m.totalCases,
-          m.totalHealed,
-          m.totalDeath,
-          r.last[0],
-        ],
-      );
-      idm = r.last[0];
     }
-    for (var d in days) {
+    days
+        .where((d) => d.annoucementDate.startsWith(m.sourceFileName))
+        .toList()
+        .forEach((d) async {
+      int idm;
       var r = await conn.query(
-          'SELECT idDay FROM dayStat WHERE annoucementDate = ?',
-          [d.annoucementDate]);
+        'SELECT idDay FROM dayStat WHERE annoucementDate = ?',
+        [d.annoucementDate],
+      );
+      var re = await conn.query(
+        "SELECT idMonth FROM monthStat WHERE sourceFileName = DATE_FORMAT(?, '%Y-%m')",
+        [d.annoucementDate],
+      );
+      idm = re.last[0];
       if (r.isEmpty) {
-        var result = await conn.query(
-          'INSERT INTO dayStat (annoucementDate,numberOfTests,numberOfNewCases,numberOfContactCases,numberOfCommunityCases,numberOfHealed,numberOfDeaths,idMonth) VALUES (?,?,?,?,?,?,?,?)',
+        loadDays(conn, d, idm);
+        await conn.query(
+          'UPDATE monthStat SET totalCases = totalCases + ?,totalHealed = totalHealed + ?,totalDeath = totalDeath + ? WHERE idMonth = ?',
           [
-            d.annoucementDate,
-            d.dayStats.numberOfTests,
-            d.dayStats.numberOfNewCases,
-            d.dayStats.numberOfCommunityCases,
-            d.dayStats.numberOfContactCases,
-            d.dayStats.numberOfHealed,
-            d.dayStats.numberOfDeaths,
-            idm
+            m.totalCases,
+            m.totalHealed,
+            m.totalDeath,
+            idm,
           ],
         );
-        for (var l in d.dayStats.localities) {
-          print(l.isRegion);
-          if (l.isRegion) {
-            continue;
-          }
-          var r = await conn.query(
-              'SELECT idLocality FROM locality WHERE localityName = ?',
-              [l.localityName]);
-          await conn.query(
-            'INSERT INTO localityStat (newCases,isRegion,idDay,idLocality) VALUES (?,?,?,?)',
-            [
-              l.newCases,
-              l.isRegion,
-              result.insertId,
-              r.last[0],
-            ],
-          );
-        }
       } else {
-        print("Déjà envoyé");
+        return showDialog(
+          context: context,
+          builder: (ctx) => ValidatorPopup(
+            title: "Risque de dupliquation",
+            message:
+                "La date ${d.annoucementDate} a déjà été envoyé dans la base",
+            question: "Voulez vous ignorer la donnée ?",
+            okCallback: () {},
+            noCallback: () async {
+              loadDays(conn, d, idm);
+            },
+          ),
+        );
       }
-    }
+    });
   }
-  await conn.close();
+}
+
+Future<void> loadDays(MySqlConnection m, Day d, int idm) async {
+  var result = await m.query(
+    'INSERT INTO dayStat (annoucementDate,numberOfTests,numberOfNewCases,numberOfContactCases,numberOfCommunityCases,numberOfHealed,numberOfDeaths,idMonth) VALUES (?,?,?,?,?,?,?,?)',
+    [
+      d.annoucementDate,
+      d.dayStats.numberOfTests,
+      d.dayStats.numberOfNewCases,
+      d.dayStats.numberOfCommunityCases,
+      d.dayStats.numberOfContactCases,
+      d.dayStats.numberOfHealed,
+      d.dayStats.numberOfDeaths,
+      idm
+    ],
+  );
+  for (var l in d.dayStats.localities) {
+    print(l.isRegion);
+    if (l.isRegion) {
+      continue;
+    }
+    var r = await m.query(
+      'SELECT idLocality FROM locality WHERE localityName = ?',
+      [l.localityName],
+    );
+    await m.query(
+      'INSERT INTO localityStat (newCases,isRegion,idDay,idLocality) VALUES (?,?,?,?)',
+      [
+        l.newCases,
+        l.isRegion,
+        result.insertId,
+        r.last[0],
+      ],
+    );
+  }
 }
